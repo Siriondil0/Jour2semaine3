9cafed4916ab6351807666fd1cc56734e729135e
Siriondil0 <elros.du.13@gmail.com>
2018-10-17 08:57:23 +0200

    a

--- main.rb
+++ main.rb
@@ -8,30 +8,33 @@ require 'tictactoe.rb'
 require "pry"
 require "rubocop"
 
+class Perform
 # demande le nom des joueurs
-def ask_name
-  puts "Bienvenue dans ce jeu de morpions. Il se joue à deux. Veuillez entrer chacun votre tour votre nom."
-  print "Nom du 1er joueur: >"
-  name1 = gets.chomp
-  print "Nom du 2nd joueur: >"
-  name2 = gets.chomp
-  puts
-  [name1,name2]
-end
+  def ask_name
+    puts "Bienvenue dans ce jeu de morpions. Il se joue à deux. Veuillez entrer chacun votre tour votre nom."
+    print "Nom du 1er joueur: >"
+    name1 = gets.chomp
+    print "Nom du 2nd joueur: >"
+    name2 = gets.chomp
+    puts
+    [name1,name2]
+  end
 
 # lance un jeu et continue a jouer si les joueurs le veulent
-def perform
-  name = ask_name
-  game1 = Game.new(name[0], name[1])
-  game_continue = "Y"
-  while game_continue =~ /Y/i
-    game1.new_game
-    puts "Voulez-vous continuer à jouer? (Y/N)"
-    print ">"
-    game_continue = gets.chomp
+  def performer
+    name = ask_name
+    game1 = Game.new(name[0], name[1])
+    game_continue = "Y"
+    while game_continue =~ /Y/i
+      game1.new_game
+      puts "Voulez-vous continuer à jouer? (Y/N)"
+      print ">"
+      game_continue = gets.chomp
+    end
+    puts "Voici le score final:"
+    game1.score
   end
-  puts "Voici le score final:"
-  game1.score
 end
 
-perform
+perform=Perform.new
+perform.performer
